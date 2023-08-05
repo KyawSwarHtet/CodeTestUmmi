92b02665c8d7e6571f7470386141a633bcd66e83
kyaw swar htet <kyawswarhtet101094@gmail.com>
2023-08-05 11:19:36 +0630

    test answer

--- dist/Test5.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var priceAfterDiscount = 1000;
-var discountPercentage = 0.2;
-var originalPrice = priceAfterDiscount / (1 - discountPercentage);
-console.log("original Price ", originalPrice);
-//# sourceMappingURL=Test5.dev.js.map
